e7ba6d56ae6520222a38bcb5f463a11051241a03
Jorge Ruesga <jorge@ruesga.com>
2012-07-21 00:56:44 +0200

    Fix Issue 5547: Apollo will not play MP3 files when using Root Explorer
    Apollo doesn't have an activity registered for support "file" schemas, that are used by file
    managers when using the method setDataAndType of Intent class.
    
    This changes adds a new activity (PlayExternal) that registers this kind of schema, and pass
    the request to AudioPlayerHolder. Allow direct play the file, or enqueue in the current track list
    
    Change-Id: I25b728700bd0d2d58d31e4de4bed81ee3e368e0a

--- src/com/andrew/apollo/service/ApolloService.java
+++ src/com/andrew/apollo/service/ApolloService.java
@@ -1138,10 +1138,16 @@ public class ApolloService extends Service {
                     where = null;
                     selectionArgs = null;
                 } else {
+                    // Remove schema for search in the database
+                    // Otherwise the file will not found
+                    String data = path;
+                    if( data.startsWith("file://") ){
+                        data = data.substring(7);
+                    }
                     uri = MediaStore.Audio.Media.getContentUriForPath(path);
                     where = MediaColumns.DATA + "=?";
                     selectionArgs = new String[] {
-                        path
+                        data
                     };
                 }
 
@@ -1183,6 +1189,48 @@ public class ApolloService extends Service {
                 mOpenFailedCounter = 0;
             }
         }
+    }
+
+    /**
+     * Method that query the media database for search a path an translate
+     * to the internal media id
+     *
+     * @param path The path to search
+     * @return long The id of the resource, or -1 if not found
+     */
+    public long getIdFromPath(String path) {
+        try {
+            // Remove schema for search in the database
+            // Otherwise the file will not found
+            String data = path;
+            if( data.startsWith("file://") ){
+                data = data.substring(7);
+            }
+            ContentResolver resolver = getContentResolver();
+            String where = MediaColumns.DATA + "=?";
+            String selectionArgs[] = new String[] {
+                data
+            };
+            Cursor cursor =
+                    resolver.query(
+                            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                            mCursorCols, where, selectionArgs, null);
+            try {
+                if (cursor == null || cursor.getCount() == 0) {
+                    return -1;
+                }
+                cursor.moveToNext();
+                return cursor.getLong(IDCOLIDX);
+            } finally {
+                try {
+                    if( cursor != null )
+                        cursor.close();
+                } catch (Exception ex) {
+                }
+            }
+        } catch (UnsupportedOperationException ex) {
+        }
+        return -1;
     }
 
     /**
@@ -1809,7 +1857,7 @@ public class ApolloService extends Service {
     public String getArtistName() {
         synchronized (this) {
             if (mCursor == null) {
-                return null;
+                return getString(R.string.unknown);
             }
             return mCursor.getString(mCursor.getColumnIndexOrThrow(AudioColumns.ARTIST));
         }
@@ -1827,7 +1875,7 @@ public class ApolloService extends Service {
     public String getAlbumName() {
         synchronized (this) {
             if (mCursor == null) {
-                return null;
+                return getString(R.string.unknown);
             }
             return mCursor.getString(mCursor.getColumnIndexOrThrow(AudioColumns.ALBUM));
         }
@@ -1845,7 +1893,7 @@ public class ApolloService extends Service {
     public String getTrackName() {
         synchronized (this) {
             if (mCursor == null) {
-                return null;
+                return getString(R.string.unknown);
             }
             return mCursor.getString(mCursor.getColumnIndexOrThrow(MediaColumns.TITLE));
         }
@@ -2122,6 +2170,11 @@ public class ApolloService extends Service {
         @Override
         public void open(long[] list, int position) {
             mService.get().open(list, position);
+        }
+
+        @Override
+        public long getIdFromPath(String path) {
+            return mService.get().getIdFromPath(path);
         }
 
         @Override
