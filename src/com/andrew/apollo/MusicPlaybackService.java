105a470b765636719dfc103e9ed6be18666a8039
Buga Dániel <bugadani@gmail.com>
2013-03-26 13:06:31 +0100

    Fix notification widget start
    When starting playback first from a widget, the notification controls aren't
    displayed. This commit should fix that and also clean up the code a bit.
    
    Change-Id: Ic8463a16dd1d930540eb8d4c2ff2e1be26666b94

--- src/com/andrew/apollo/MusicPlaybackService.java
+++ src/com/andrew/apollo/MusicPlaybackService.java
@@ -383,7 +383,7 @@ public class MusicPlaybackService extends Service {
     /**
      * Returns true if the Apollo is sent to the background, false otherwise
      */
-    private boolean mBuildNotification = false;
+    private boolean mBuildNotification;
 
     /**
      * Lock screen controls ICS+
@@ -650,8 +650,11 @@ public class MusicPlaybackService extends Service {
         if (intent != null) {
             final String action = intent.getAction();
 
-            if (FOREGROUND_STATE_CHANGED.equals(action)) {
+            if (intent.hasExtra(NOW_IN_FOREGROUND)) {
                 mBuildNotification = !intent.getBooleanExtra(NOW_IN_FOREGROUND, false);
+            }
+
+            if (FOREGROUND_STATE_CHANGED.equals(action)) {
                 if (mBuildNotification && isPlaying()) {
                     buildNotification();
                 } else if (!mBuildNotification) {
