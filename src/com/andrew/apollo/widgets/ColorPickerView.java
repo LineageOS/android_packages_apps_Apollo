bb9f093e588cb7debbf98b475151c3986efd5cc8
Jorge Ruesga <jorge@ruesga.com>
2013-03-31 23:55:41 +0200

    Apollo: Disable hardware acceleration in ColorPickerView
    http://developer.android.com/guide/topics/graphics/hardware-accel.html
    
    << ComposeShader can only contain shaders of different types (a BitmapShader and a
    LinearGradient for instance, but not two instances of BitmapShader)'. But, 'If your
    application is affected by any of these missing features or limitations, you can turn
    off hardware acceleration for just the affected portion of your application by calling
    setLayerType(View.LAYER_TYPE_SOFTWARE, null). >>
    
    Change-Id: I6979051a8d8e4f6cd3dc5ad5dc4736c3adf8ec79
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/andrew/apollo/widgets/ColorPickerView.java
+++ src/com/andrew/apollo/widgets/ColorPickerView.java
@@ -28,6 +28,8 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.lang.reflect.Method;
+
 /**
  * Displays a color picker to the user and allow them to select a color. A
  * slider for the alpha channel is also available. Enable it by setting
@@ -223,6 +225,12 @@ public class ColorPickerView extends View {
         }
 
         return hue;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        checkHardwareAccelerationSupport();
     }
 
     @Override
@@ -944,4 +952,34 @@ public class ColorPickerView extends View {
     public String getAlphaSliderText() {
         return mAlphaSliderText;
     }
+
+    /**
+     * Method that checks the support for HardwareAcceleration. Check AOSP notice<br/>
+     * <br/>
+     * <pre>
+     * 'ComposeShader can only contain shaders of different types (a BitmapShader and a
+     * LinearGradient for instance, but not two instances of BitmapShader)'. But, 'If your
+     * application is affected by any of these missing features or limitations, you can turn
+     * off hardware acceleration for just the affected portion of your application by calling
+     * setLayerType(View.LAYER_TYPE_SOFTWARE, null).'
+     */
+    private void checkHardwareAccelerationSupport() {
+       // HardwareAcceleration sit is only available since ICS. 14 = ICS_VERSION_CODE
+       if (android.os.Build.VERSION.SDK_INT >= 14) {
+           try{
+               // We need to use reflection to get that method to avoid compilation errors
+               Method isHardwareAccelerated =
+                       getClass().getMethod("isHardwareAccelerated", new Class[]{});
+               Object o = isHardwareAccelerated.invoke(this, new Object[]{});
+               if (null != o && o instanceof Boolean && (Boolean)o) {
+                   // HardwareAcceleration is supported. Use SoftwareAcceleration
+                   Method setLayerType =
+                           getClass().getMethod(
+                                   "setLayerType", int.class, android.graphics.Paint.class);
+                   setLayerType.invoke(this, 1, (android.graphics.Paint)null);
+               }
+           } catch (Exception e) { /** NON BLOCK **/}
+       }
+   }
+
 }
