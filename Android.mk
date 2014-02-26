06dbd18c15313a6976f321528c1ea786523f3bbc
BuzzBumbleBee <bigbeeshane@gmail.com>
2014-02-26 10:01:48 +0000

    wipJam : Change the name of Apollo
    This is from wipJam
    
    Change-Id: I6718772222e931150fddbf88a28dcd2f771bf010

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES += $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4
 
-LOCAL_PACKAGE_NAME := Apollo
+LOCAL_PACKAGE_NAME := Apollo1
 LOCAL_OVERRIDES_PACKAGES := Music
 
 LOCAL_SDK_VERSION := current
