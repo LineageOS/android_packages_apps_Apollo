8a8c2267afcbf5eb922537c39a2ace81ef657eda
Shareef Ali <shareefalis@gmail.com>
2014-08-15 20:35:38 -0400

    Apollo: add error handling to a invalid file.
    *  can occure if you make changes to the file itself
    * JIRA: BUGDUMP-1050
    
    Change-Id: Ia8215c3643b1eb7f8b5786eeb8267340b673939c

--- src/com/andrew/apollo/utils/MusicUtils.java
+++ src/com/andrew/apollo/utils/MusicUtils.java
@@ -602,7 +602,10 @@ public final class MusicUtils {
         try {
             mService.stop();
             mService.openFile(filename);
-            mService.play();
+            try {
+            	mService.play();
+            }catch (IllegalArgumentException ignored){
+            }
         } catch (final RemoteException ignored) {
         }
     }
